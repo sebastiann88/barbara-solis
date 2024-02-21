7942a32cab7b7160a3207f01778e7d7ffb04a0b4
sebastiann88 <sebastian.nadeau45@gmail.com>
2024-02-21 09:38:47 -0500

    feat(all): configure tina cms

--- tina/config.ts
+++ tina/config.ts
@@ -1,23 +1,19 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const branch = "main";
 
 export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: "b6b3d5aa-e419-4078-abb0-b4c83853d434",
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: '455a6fbadab3dba70040a4ee42dda8a819376af1',
 
   build: {
     outputFolder: "admin",
-    publicFolder: "static",
+    publicFolder: "public",
   },
   media: {
     tina: {
@@ -55,11 +51,28 @@ export default defineConfig({
         format: "md",
         fields: [
           {
-            name: "title",
             type: "string",
+            name: "title",
+            label: "title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
           },
         ],
       },
     ],
+  },
+  search: {
+    tina: {
+      indexerToken: '7c8e37d8feafd0b27b0109e7fc39f9ded5d48c9d',
+      stopwordLanguages: ['eng'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
   },
 });
