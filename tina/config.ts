5be2710f5adc32ad6a76a528fc9f78605e692368
sebastiann88 <sebastian.nadeau45@gmail.com>
2024-02-21 10:06:03 -0500

    refactor(ts): update media file

--- tina/config.ts
+++ tina/config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "static",
+      mediaRoot: "static/img",
+      publicFolder: "public",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
