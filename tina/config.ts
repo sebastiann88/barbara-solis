8a431d91eecd57a0e068eb16729b067ae37da92c
sebastiann88 <sebastian.nadeau45@gmail.com>
2024-02-21 20:24:32 -0500

    refactor(ts, json): hide posts

--- tina/config.ts
+++ tina/config.ts
@@ -27,26 +27,26 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
+      // {
+      //   name: "post",
+      //   label: "Posts",
+      //   path: "content/posts",
+      //   fields: [
+      //     {
+      //       type: "string",
+      //       name: "title",
+      //       label: "Title",
+      //       isTitle: true,
+      //       required: true,
+      //     },
+      //     {
+      //       type: "rich-text",
+      //       name: "body",
+      //       label: "Body",
+      //       isBody: true,
+      //     },
+      //   ],
+      // },
       {
         name: "page",
         label: "Page",
