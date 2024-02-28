e9a53608d7ba99fb02c4e381c1ebd14a2d176c93
sebastiann88 <sebastian.nadeau45@gmail.com>
2024-02-28 13:38:38 -0500

    refactor(all): change asset folder

--- tina/config.ts
+++ tina/config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
 
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "static",
   },
   media: {
     tina: {
       mediaRoot: "assets",
-      publicFolder: "public",
+      publicFolder: "static",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
